8e106fc7194ee2e34818dd8c375aac5209750359
louisstow <louisstow@gmail.com>
2014-02-11 15:53:53 +1000

    * Initial commit

---
+++ user.js
@@ -0,0 +1,17 @@
+pref('browser.manifestURL', "app://system.gaiamobile.org/manifest.webapp");
+pref('b2g.neterror.url', "app://system.gaiamobile.org/net_error.html");
+pref('browser.homescreenURL', "app://system.gaiamobile.org/index.html");
+pref('network.http.max-connections-per-server', 15);
+pref('dom.mozInputMethod.enabled', true);
+pref('ril.debugging.enabled', false);
+pref('dom.mms.version', 17);
+pref('b2g.wifi.allow_unsafe_wpa_eap', true);
+pref('network.dns.localDomains', "gaiamobile.org,gallery.gaiamobile.org,search.gaiamobile.org,costcontrol.gaiamobile.org,pdfjs.gaiamobile.org,settings.gaiamobile.org,wappush.gaiamobile.org,sms.gaiamobile.org,ringtones.gaiamobile.org,browser.gaiamobile.org,email.gaiamobile.org,communications.gaiamobile.org,music.gaiamobile.org,homescreen.gaiamobile.org,video.gaiamobile.org,fl.gaiamobile.org,keyboard.gaiamobile.org,system.gaiamobile.org,bluetooth.gaiamobile.org,setringtone.gaiamobile.org,fm.gaiamobile.org,clock.gaiamobile.org,calendar.gaiamobile.org,wallpaper.gaiamobile.org,camera.gaiamobile.org,marketplace.firefox.com.gaiamobile.org");
+pref("geo.gps.supl_server", "supl.izatcloud.net");
+pref("geo.gps.supl_port", 22024);
+pref("dom.payment.provider.0.name", "firefoxmarket");
+pref("dom.payment.provider.0.description", "marketplace.firefox.com");
+pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
+pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
+pref("dom.payment.provider.0.requestMethod", "GET");
+pref("media.navigator.permission.disabled", false);
